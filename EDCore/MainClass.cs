21cc1c8a5a2b990d5137b32f7df6e8620ec8d02e
MyBlueHorizon <tianjizhishang@outlook.com>
2023-12-07 21:53:19 +0800

    安装软件包。

---
+++ EDCore/MainClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDCore
+{
+    public class MainClass
+    {
+        public static CrateClient
+        {
+            var scopes = new[] { "User.Read" };
+
+            // Multi-tenant apps can use "common",
+            // single-tenant apps must use the tenant ID from the Azure portal
+            var tenantId = "common";
+
+            // Value from app registration
+            var clientId = "YOUR_CLIENT_ID";
+
+            // using Azure.Identity;
+            var options = new DeviceCodeCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+                ClientId = clientId,
+                TenantId = tenantId,
+                // Callback function that receives the user prompt
+                // Prompt contains the generated device code that user must
+                // enter during the auth process in the browser
+                DeviceCodeCallback = (code, cancellation) =>
+                {
+                    Console.WriteLine(code.Message);
+                    return Task.FromResult(0);
+                },
+            };
+
+            // https://learn.microsoft.com/dotnet/api/azure.identity.devicecodecredential
+            var deviceCodeCredential = new DeviceCodeCredential(options);
+
+            var graphClient = new GraphServiceClient(deviceCodeCredential, scopes);
+        }
+    }
+}
