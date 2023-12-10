0f8c10b215e2628a0c8ebee96e08b345f95d0143
MyBlueHorizon <tianjizhishang@outlook.com>
2023-12-10 15:02:26 +0800

    初始化登录UI。

--- EveryDrive/MainWindow.xaml.vb
+++ EveryDrive/MainWindow.xaml.vb
@@ -1,3 +1,9 @@
-﻿Class MainWindow
-
+﻿Imports EDCore.MainClass
+Class MainWindow
+    ReadOnly MyEDCore As New EDCore.MainClass
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        MyEDCore.CrateClient(by.Text， az.Text)
+        Dim userDisplayName = MyEDCore.getUserInformationAsync.Result
+        DisplayName.Text = userDisplayName
+    End Sub
 End Class
