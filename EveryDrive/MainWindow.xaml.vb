db709555fe58e81f5f28cc9d6747086c00891bbd
MyBlueHorizon <tianjizhishang@outlook.com>
2023-12-13 17:59:26 +0800

    UI修改。

--- EveryDrive/MainWindow.xaml.vb
+++ EveryDrive/MainWindow.xaml.vb
@@ -1,9 +1,16 @@
-﻿Imports EDCore.MainClass
+﻿Imports Microsoft.Graph
 Class MainWindow
-    ReadOnly MyEDCore As New EDCore.MainClass
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        MyEDCore.CrateClient(by.Text， az.Text)
-        Dim userDisplayName = MyEDCore.getUserInformationAsync.Result
-        DisplayName.Text = userDisplayName
+        EDCore.MainClass.InitializeGraphModule()
+        Dim UserInfo = EDCore.MainClass.GetUserInformationAsync().Result
+        USERNAME.Content = $"用户名, {UserInfo?.DisplayName}!"
+        EMAIL.Content = $"邮箱: {UserInfo?.Mail & UserInfo?.UserPrincipalName}"
+        'If UserInfo.GetType.Name = "String" Then
+        '    MsgBox(UserInfo)
+        'Else
+        '    USERNAME.Content = $"用户名, {UserInfo?.DisplayName}!"
+        '    EMAIL.Content = $"邮箱: {UserInfo?.Mail & UserInfo?.UserPrincipalName}"
+        'End If
+
     End Sub
 End Class
