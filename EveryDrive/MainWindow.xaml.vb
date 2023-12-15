004d491f8f7526d3f8e83228ff04c9e5f85ff3d7
MyBlueHorizon <tianjizhishang@outlook.com>
2023-12-15 19:50:57 +0800

    移除断点。

--- EveryDrive/MainWindow.xaml.vb
+++ EveryDrive/MainWindow.xaml.vb
@@ -5,6 +5,7 @@ Class MainWindow
         Dim UserInfo = EDCore.MainClass.GetUserInformationAsync().Result
         USERNAME.Content = $"用户名, {UserInfo?.DisplayName}!"
         EMAIL.Content = $"邮箱: {UserInfo?.Mail & UserInfo?.UserPrincipalName}"
+        MsgBox(UserInfo)
         'If UserInfo.GetType.Name = "String" Then
         '    MsgBox(UserInfo)
         'Else
